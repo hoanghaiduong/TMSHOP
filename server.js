5170f1247fd18d857dc885b5ec66f74029b93aa5
hoanghaiduong <hoanghaiduong1711@gmail.com>
2024-12-15 11:43:25 +0700

    Final2

--- server.js
+++ server.js
@@ -63,7 +63,7 @@ mongoose
 
 app.use(express.static("./client/build"));
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
 });
 
 app.use("/api/auth", authRouter);
