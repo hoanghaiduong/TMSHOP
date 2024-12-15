4b37adc05d4b7dd8f33ac1abc4929f7398656702
hoanghaiduong <hoanghaiduong1711@gmail.com>
2024-12-15 11:18:26 +0700

    ok

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const path = require("path");
+require("dotenv").config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 const allowedOrigins = [
@@ -34,6 +35,8 @@ app.use(cookieParser());
 app.use(express.json());
 // app.use(express.static(path.join(__dirname, "public")));
 app.use("/", (req, res) => {
+  console.log(PORT);
+
   return res.status(200).json({
     message: "Hello World",
   });
